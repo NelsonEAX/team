9b514432c49c216a525f4e75e2f848bc669e7d84
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-20 17:05:35 +0000

    Deploy NelsonEAX/team to github.com/NelsonEAX/team.git:gh-pages

---
+++ precache-manifest.a553446428de7cf81e3a84e490bb8ff4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f58b386f22b0627358f",
+    "url": "/team/js/about.194c56b6.js"
+  },
+  {
+    "revision": "b2fc38c8c4a9ea239415",
+    "url": "/team/css/app.aded043c.css"
+  },
+  {
+    "revision": "b2fc38c8c4a9ea239415",
+    "url": "/team/js/app.0c7cea66.js"
+  },
+  {
+    "revision": "31da31031a835c2e6969",
+    "url": "/team/js/chunk-vendors.ac1416ca.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/team/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "59b7d8b77d12b976661e6965f5cadd60",
+    "url": "/team/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/team/robots.txt"
+  }
+];
